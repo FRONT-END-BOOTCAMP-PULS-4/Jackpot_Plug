93aed7d4afef724c5d562efae83a3fed6c96d694
haSong <70552536+poan1221@users.noreply.github.com>
2025-04-14 14:12:43 +0900

    style. 기본 스타일 설정

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./global.scss";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: LayoutProps) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>PLUG : 나만의 플레이리스트 </title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
