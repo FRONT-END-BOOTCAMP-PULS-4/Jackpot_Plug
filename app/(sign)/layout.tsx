354cac65b28154b5b31b1b23fbee75b059464163
haSong <70552536+poan1221@users.noreply.github.com>
2025-04-21 13:51:43 +0900

    fix : import 오류 수정

--- app/(sign)/layout.tsx
+++ app/(sign)/layout.tsx
@@ -1,29 +1,9 @@
-import Toast from "../components/toast/Toast";
-
 import { LayoutProps } from "../layout";
 
 export default function signLayout({ children }: LayoutProps) {
   return (
-    <>
-      <div
-        id="toast-root"
-        style={{
-          position: "fixed",
-          zIndex: 9999,
-          pointerEvents: "none",
-        }}
-      />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          height: "100vh",
-        }}
-      >
-        <main>{children}</main>
-      </div>
-    </>
+    <div className="sign_layout">
+      <main>{children}</main>
+    </div>
   );
 }
