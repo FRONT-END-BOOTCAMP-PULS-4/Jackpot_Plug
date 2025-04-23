ab40245da5bf27669aec6286b58cd15103302398
haSong <70552536+poan1221@users.noreply.github.com>
2025-04-23 23:23:57 +0900

    refactor: 파일명 변경

---
+++ app/api/musics/[videoId]/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { SearchMusicUseCase } from "@/application/usecases/search/SearchMusicUseCase";
+
+export async function GET(request: Request) {
+  try {
+    const url = new URL(request.url);
+    const query = url.searchParams.get("q");
+
+    console.log("서버에서 받은 검색어:", query);
+
+    // const searchMusicUseCase = new SearchMusicUseCase();
+    // return await searchMusicUseCase.execute(query || "");
+  } catch (error) {
+    console.error("Error in route handler:", error);
+    return NextResponse.json(
+      { error: "An error occurred while processing your request" },
+      { status: 500 }
+    );
+  }
+}
