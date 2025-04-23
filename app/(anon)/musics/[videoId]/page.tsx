ccba84144886214503c231ae82453d511bf8bb6e
haSong <70552536+poan1221@users.noreply.github.com>
2025-04-23 15:50:52 +0900

    feat: useVideoExtract 로 커스텀훅 분리

--- app/(anon)/musics/[videoId]/page.tsx
+++ app/(anon)/musics/[videoId]/page.tsx
@@ -7,6 +7,8 @@ import VideoExtractor from "../../components/VideoExtractor";
 import MusicInfoCard from "../../components/musicInfoCard/MusicInfoCard";
 import ListItem from "@/app/components/list/ListItem";
 import { RoundBtn } from "@/app/components/button/Buttons";
+import RouteModal from "@/app/components/modal/RouteModal";
+import { useVideoExtract } from "@/hooks/useVideoExtract";
 
 interface VideoData {
   title: string;
@@ -21,74 +23,77 @@ export default function MusicPage() {
   const { videoId } = useParams() as { videoId: string };
   const [data, setData] = useState<VideoData | null>(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
   const [isLogin, setIsLogin] = useState(false);
+  const { extract, routeModal, modalMessage } = useVideoExtract();
 
   useEffect(() => {
     const cached = sessionStorage.getItem(videoId);
 
     if (cached) {
       setData(JSON.parse(cached));
-      console.log("캐시된 데이터 사용");
       setLoading(false);
     } else {
-      fetch(`/api/videos?id=${videoId}`)
-        .then((res) => res.json())
-        .then((resData) => {
-          if (resData.error) {
-            setError(resData.error);
-          } else {
-            sessionStorage.setItem(videoId, JSON.stringify(resData));
-            setData(resData);
-          }
-        })
-        .catch(() => {
-          setError("데이터를 불러오는 데 실패했습니다.");
-        })
-        .finally(() => setLoading(false));
+      extract({
+        videoId,
+        onSuccess: (data) => {
+          setData(data);
+          setLoading(false);
+        },
+      });
     }
   }, [videoId]);
 
-  if (loading) return <p>로딩 중...</p>;
-  if (error) return <p>{error}</p>;
-  if (!data) return <p>데이터 없음</p>;
-
   return (
-    <div>
-      <VideoExtractor defaultUrl={data.videoUrl} size="small" />
-      <div className={styles.container}>
-        <MusicInfoCard
-          thumbnailSrc={data.thumbnail}
-          title={data.title}
-          channelTitle={data.channelTitle}
-          duration={data.duration}
-        />
-        <div className={styles.musiclist_container}>
-          <ul className={styles.musiclist}>
-            {data.musicList.map((music, index) => (
-              <ListItem
-                mode="extract"
-                title={music}
-                key={index}
-                isLogin={isLogin}
-              />
-            ))}
-          </ul>
-          <div className={styles.button_container}>
-            {!isLogin && (
-              <div className={styles.animation}>
-                로그인 하면 사용할 수 있어요! 👉
-              </div>
-            )}
-            <RoundBtn
-              text="플레이리스트 만들기"
-              size="md"
-              color="accent"
-              customClassName={!isLogin ? "disabled" : ""}
+    <>
+      {loading ? (
+        <p>로딩 중...</p>
+      ) : !data ? (
+        <p>데이터 없음</p>
+      ) : (
+        <div>
+          <VideoExtractor defaultUrl={data.videoUrl} size="small" />
+          <div className={styles.container}>
+            <MusicInfoCard
+              thumbnailSrc={data.thumbnail}
+              title={data.title}
+              channelTitle={data.channelTitle}
+              duration={data.duration}
             />
+            <div className={styles.musiclist_container}>
+              <ul className={styles.musiclist}>
+                {data.musicList.map((music, index) => (
+                  <ListItem
+                    mode="extract"
+                    title={music}
+                    key={index}
+                    isLogin={isLogin}
+                  />
+                ))}
+              </ul>
+              <div className={styles.button_container}>
+                {!isLogin && (
+                  <div className={styles.animation}>
+                    로그인 하면 사용할 수 있어요! 👉
+                  </div>
+                )}
+                <RoundBtn
+                  text="플레이리스트 만들기"
+                  size="md"
+                  color="accent"
+                  customClassName={!isLogin ? "disabled" : ""}
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+      <RouteModal
+        isOpen={routeModal.isOpen}
+        onClose={routeModal.close}
+        buttonText="네, 검색하러 갈게요."
+        targetRoute="/search"
+        message={modalMessage}
+      />
+    </>
   );
 }
