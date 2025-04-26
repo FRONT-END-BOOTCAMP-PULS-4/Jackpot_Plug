fb2c99db04d726c6c59ae1754e59e87992d2c1eb
HeekunLim <fanghk555@gmail.com>
2025-04-26 19:00:34 +0900

    style: 메일 내용 수정

--- app/(sign)/layout.tsx
+++ app/(sign)/layout.tsx
@@ -1,7 +1,9 @@
+// app\(sign)\layout.tsx
 "use client";
 
 import { useRouter } from "next/navigation";
 import { LayoutProps } from "../layout";
+import styles from "./page.module.scss";
 
 export default function signLayout({ children }: LayoutProps) {
   const router = useRouter();
@@ -10,10 +12,13 @@ export default function signLayout({ children }: LayoutProps) {
     <div className="sign_layout">
       <div className="sign_header">
         <button onClick={() => router.back()}>
-          <span>←Back</span>
+          <span>← Back</span>
         </button>
       </div>
       <main className="sign_main">{children}</main>
+      <footer className={styles.sign_footer}>
+        <p>© Plug. 2025 All rights reserved.</p>
+      </footer>
     </div>
   );
 }
